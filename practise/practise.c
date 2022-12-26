e45fce6a091f613cc2347c4842de6160361ff6d4
ARUN-ROOT <arunvenkatesan25@gmail.com>
2022-12-26 17:29:48 +0530

    practise linux code

---
+++ practise/practise.c
@@ -0,0 +1,27 @@
+/* 
+Name			:Array of  Function Pointer
+Date			: 25/12/2022
+Author			: Arun.V
+Code link		:
+Descrition		: 
+ o/p			:
+ */
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main()
+{
+  if(fork())
+  {
+     printf("1\n");
+  }
+  printf("2\n");
+
+/*
+  int a=2,result;
+  result = a && a || a ;
+  printf("%d\n",result);
+  */
+}
---
+++ practise/structureFn2.c
@@ -0,0 +1,30 @@
+/* 
+Name	: Structure and Function
+Date	: 22/12/2022
+Descrition : Passing structure member address as argument to function under Normal structure variable stu;
+ o/p	:10
+
+
+ */
+
+
+#include <stdio.h>
+void passing(int *i);
+struct student
+{
+  int i;
+};
+
+    
+int main()
+{  
+   struct student stu;
+   stu.i=10;
+   passing(&(stu.i));//passing value of structure member
+
+}
+
+void passing(int *i)
+{
+  printf("%d\n",*i);
+}
